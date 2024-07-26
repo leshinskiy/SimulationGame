ca899c42a4884c7068c663a41df96a1be3bc3230
kirusha :) <kirilllet209@gmail.com>
2024-07-26 07:29:07 +0300

    simulation v1.0
    first  working version of simulation

--- src/com/leshinskiy/main/Main.java
+++ src/com/leshinskiy/main/Main.java
@@ -1,11 +1,11 @@
-//package com.leshinskiy.main;
-//
-//public class Main {
-//
-//    public static void com.leshinskiy.main(String[] args) {
-//        Simulation simulation = new Simulation();
-//        simulation.startSimulation();
-//    }
-//}
-//
-//
+package com.leshinskiy.main;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Simulation simulation = new Simulation();
+        simulation.startSimulation();
+    }
+}
+
+
--- src/com/leshinskiy/main/Simulation.java
+++ src/com/leshinskiy/main/Simulation.java
@@ -1,24 +1,61 @@
 package com.leshinskiy.main;
 
+import com.leshinskiy.action.CreateAction;
 import com.leshinskiy.action.InitAction;
 import com.leshinskiy.action.TurnAction;
 
+import java.util.Scanner;
+
 public class Simulation {
+    private int turnCounter;
+    private boolean gameStatus = false;
+    GameMap gameMap = new GameMap();
+    Renderer renderer = new Renderer();
+    InitAction initAction = new InitAction();
+    TurnAction turnAction = new TurnAction();
     public void startSimulation(){
-        GameMap gameMap = new GameMap();
-        Renderer renderer = new Renderer();
-        InitAction init = new InitAction();
-        TurnAction turn = new TurnAction();
-        init.initializeMap(gameMap);
+        initAction.initializeMap(gameMap);
 
+        gameStatus = true;
+        while(gameStatus) {
+            nextTurn();
+            try {
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("Если вы хотите продолжить нажмите Enter или введите любой символ\nЕсли вы хотите остановить симуляцию введите Y");
+                String playerStatus;
 
+                do {
+                    playerStatus = scanner.nextLine();
+                } while (playerStatus.length() > 1);
 
-        int i = 0;
-        while(true){
-            renderer.renderField(gameMap);
-            turn.makeTurn(gameMap);
-            System.out.println();
+                if("y".equalsIgnoreCase(playerStatus)) {
+                    pauseSimulation();
+                } else {
+                    System.out.println("Продолжаю игру...\n");
+                }
+
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
+    }
+
+    public void nextTurn() {
+        turnCounter++;
+        System.out.println("Количество ходов: " + turnCounter + "\n");
+
+        renderer.renderField(gameMap);
+
+        turnAction.makeTurn(gameMap);
+
 
     }
+
+    public void pauseSimulation() {
+        System.out.println("Симуляция остановлена...");
+        gameStatus = false;
+    }
+
+
 }
