2058f911d0bdfed2498a9d60542ace1cb9b7eab4
kirusha :) <kirilllet209@gmail.com>
2024-07-26 07:30:24 +0300

    now uses create action to create creatures

--- src/com/leshinskiy/action/TurnAction.java
+++ src/com/leshinskiy/action/TurnAction.java
@@ -1,5 +1,6 @@
 package com.leshinskiy.action;
 
+import com.leshinskiy.entity.Entity;
 import com.leshinskiy.entity.Grass;
 import com.leshinskiy.entity.creature.Creature;
 import com.leshinskiy.entity.creature.herbivores.Herbivore;
@@ -10,34 +11,43 @@ import java.util.ArrayList;
 
 public class TurnAction {
     public void makeTurn(GameMap gameMap) {
+
         ArrayList<Creature> creatures = gameMap.arrayOfCreatures();
+
+        isGrassOnMap(gameMap);
+        isHerbivoresOnMap(gameMap);
 
         for(Creature creature : creatures) {
             creature.makeMove(gameMap);
         }
 
-        //isGrassOnMap(gameMap);
 
-        //isHerbivoresOnMap(gameMap);
 
     }
 
     private void isGrassOnMap(GameMap gameMap) {
+        CreateAction createAction = new CreateAction();
         ArrayList<Grass> grass = gameMap.arrayOfGrass();
 
         if(grass.size() < 3) {
-            Coordinates coordinates = gameMap.getFreeCoordinates();
-            gameMap.addEntity(new Grass(coordinates), coordinates);
+            createAction.spawnGrass(gameMap);
+            createAction.spawnGrass(gameMap);
+            createAction.spawnGrass(gameMap);
         }
 
     }
 
     private void isHerbivoresOnMap(GameMap gameMap) {
+        CreateAction createAction = new CreateAction();
         ArrayList<Herbivore> herbivores = gameMap.arrayOfHerbivore();
 
-        if(herbivores.size() < 3) {
-
-
+        if(herbivores.isEmpty()) {
+            createAction.spawnHerbivore(gameMap);
+            createAction.spawnHerbivore(gameMap);
+            createAction.spawnHerbivore(gameMap);
+            createAction.spawnHerbivore(gameMap);
         }
     }
+
+
 }
